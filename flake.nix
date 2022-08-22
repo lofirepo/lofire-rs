b9ed1647ec1d3ff8f15bf717e1507990c816cf93
TG × ⊙ <*@tg-x.net>
2022-08-22 15:46:41 +0200

    nix flake: format code with alejandra

--- flake.nix
+++ flake.nix
@@ -5,28 +5,34 @@
   inputs.utils.url = "github:numtide/flake-utils";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = { self, nixpkgs, utils, rust-overlay }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [
-          (import rust-overlay)
-        ];
-        pkgs = import nixpkgs rec {
-          inherit system overlays;
-        };
-        rust = pkgs.rust-bin.stable."1.62.0".default.override {
-          extensions = [ "rust-src" ];
-        };
-        buildRustPackage = (pkgs.makeRustPlatform {
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    rust-overlay,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        (import rust-overlay)
+      ];
+      pkgs = import nixpkgs rec {
+        inherit system overlays;
+      };
+      rust = pkgs.rust-bin.stable."1.62.0".default.override {
+        extensions = ["rust-src"];
+      };
+      buildRustPackage =
+        (pkgs.makeRustPlatform {
           cargo = rust;
           rustc = rust;
-        }).buildRustPackage;
-        myNativeBuildInputs = with pkgs; [
-        ];
-        myBuildInputs = with pkgs; [
-        ];
-        myBuildRustPackage = attrs:
-          buildRustPackage ({
+        })
+        .buildRustPackage;
+      myNativeBuildInputs = with pkgs; [
+      ];
+      myBuildInputs = with pkgs; [
+      ];
+      myBuildRustPackage = attrs:
+        buildRustPackage ({
             version = "0.1.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
@@ -34,15 +40,16 @@
             buildInputs = myBuildInputs;
             #RUST_BACKTRACE=1;
             #RUST_LOG="trace";
-          } // attrs);
-      in rec {
-        packages = rec {
-          lofire-repo = myBuildRustPackage rec {
-            pname = "lofire-repo";
-            buildAndTestSubdir = "./lofire-repo";
-          };
-          default = lofire-repo;
+          }
+          // attrs);
+    in rec {
+      packages = rec {
+        lofire-repo = myBuildRustPackage rec {
+          pname = "lofire-repo";
+          buildAndTestSubdir = "./lofire-repo";
         };
-        defaultPackage = packages.default; # compat
-      });
+        default = lofire-repo;
+      };
+      defaultPackage = packages.default; # compat
+    });
 }
